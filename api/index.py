8392bc3b7f26e4a37ec5175836052dd80c8798d9
VijaykishanK <sixplay05@gmail.com>
2026-02-20 13:29:35 +0100

    new update

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import os
+import sys
+
+# Add root directory to sys.path so 'backend' can be imported
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.main import app
